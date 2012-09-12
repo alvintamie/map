5dc66f64c66d4551ac6667f6f1be6a4e153729b2
alvi0010 <alvintamie@gmail.com>
2012-09-13 02:01:16 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -34,7 +34,8 @@ function insertReference() {
 	temp.setAttribute('id', "contentReference");
 	temp.style.position = 'absolute';
 	temp.style.top = topbarHeight + 'px';
-	temp.style.overflow = 'scroll';
+	temp.style['overflow-x'] = 'hidden';
+	temp.style['overflow-y'] = 'auto';
 	
 	while (readyRef==0) {waiting(500); console.log("wait");}
 	if (readyRef==1) {
