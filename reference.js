43f5778ca413404bd23c6bc2a1c408a2e39ec222
alvi0010 <alvintamie@gmail.com>
2012-09-12 23:57:54 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -25,6 +25,26 @@ function mouseDownReference(e){
 		}
 	}
 }
+
+function insertReference() {
+	var temp = document.createElement('div');
+	temp.setAttribute('id', "contentReference");
+	temp.style.height = topbarHeight + 'px';
+	document.getElementById("divReference").appendChild(temp);
+	while (readyRef==0) waiting(500);
+	if (readyRef==1) {
+		for (var i=0; i<numberRef; i++) {
+			temp = document.createElement('div');
+			temp.setAttribute('id', "Reference" + i);
+			document.getElementById("divReference").appendChild(temp);
+			document.getElementById("Reference"+i).innerHTML = referenceObject[i]5.title;
+		}
+	}
+	else if (readyRef==2) {
+		document.getElementById("contentReference").innerHTML = "The paper is not published yet.";
+	}
+	
+}
 	
 function changeViewReference() {
 	if (referenceStatus <= 0 && referenceIncrement==-1) {
