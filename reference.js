fff57b3b253d1b80566aa9fec1753f465ee01a51
alvi0010 <alvintamie@gmail.com>
2012-09-15 04:09:21 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -74,7 +74,7 @@ function insertReference(i) {
 	var temp = document.createElement("IMG");
 	temp.setAttribute('id', "Reference" + i + "_image");
 	temp.src = imgExpand.src;
-	temp.setAttribute('onclick', "showAbstractRef(i)");
+	temp.setAttribute('onclick', "showAbstractRef("+i+")");
 	document.getElementById("Reference"+i).appendChild(temp);
 	temp = document.createElement("a");
 	temp.href = "javascript:window.open('" + referenceObject[i].url + "')";
