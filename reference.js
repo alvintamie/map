7e9974aecc3dbbc00981bca66681cdb64eb1b6eb
alvi0010 <alvintamie@gmail.com>
2012-09-15 04:24:23 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -97,7 +97,7 @@ function insertReference(i) {
 }
 
 function showAbstractRef(i) {
-	console.log(i);
+	//console.log(i);
 	if (abstractRefMode[i]==0) {
 		document.getElementById("Reference" + i + "_image").src = imgContract.src;
 		document.getElementById("Reference" + i + "_abstract").style.display = 'block';
