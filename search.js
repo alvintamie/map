1f91224d0d712fccfa2226111dfaeaae6f39cc44
alvi0010 <alvintamie@gmail.com>
2012-09-18 00:32:57 +0800

    Update search.js

--- search.js
+++ search.js
@@ -54,14 +54,13 @@ function updatecontentSearch_query() {
 	searchText.type = 'text';
 	searchText.setAttribute('name', 'search_inputText');
 	searchForm.appendChild(searchText);
-	/*
+	
 	var searchSelect = document.createElement('select');
 	searchSelect.setAttribute('name', 'search_inputSelect')
 	for (var i=0; i<searchElement.length; i++) {
-		searchSelect.options[i].value = i;
-		searchSelect.options[i].text = searchElement[i];
+		searchSelect.options[i] = new Option (searchElement[i], i);
 	}
-	searchForm.appendChild(searchSelect);*/
+	searchForm.appendChild(searchSelect);
 }
 
 function removecontentSearch_queryChild() {
