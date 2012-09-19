fa8388360a25ba943f351c7fd4e1fe538a894fbe
alvi0010 <alvintamie@gmail.com>
2012-09-19 23:00:42 +0800

    Update mainCanvas.js

--- mainCanvas.js
+++ mainCanvas.js
@@ -12,7 +12,7 @@ function initializeMainCanvas(){
 			mouseDownCoords = [e.clientX - imageCoords[0], e.clientY - imageCoords[1]];
 			startCoords = [(e.clientX-canvasPosX) - last[0],(e.clientY-canvasPosY) - last[1]];
 			var clickMapCoords = [(mouseDownCoords[0]-canvas.offsetLeft)%img[zoom].width, (mouseDownCoords[1]-canvas.offsetTop)%img[zoom].height];
-			if (clickMapCoords[0] < 0) clickMapCoords += img[zoom].width;
+			if (clickMapCoords[0] < 0) clickMapCoords[0] += img[zoom].width;
 			console.log(clickMapCoords[0]);
 		//	if (clickMapCoords[1] < 0) clickMapCoords += img[zoom].height;
 			listenAllClick(clickMapCoords[0],clickMapCoords[1], 0);
