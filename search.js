a8d51b742a950889ceae191206367139a917e343
alvi0010 <alvintamie@gmail.com>
2012-09-20 00:13:53 +0800

    Update search.js

--- search.js
+++ search.js
@@ -226,6 +226,7 @@ function submitSearchQuery() {
 		searchBoolQuery[0] = 0;
 		for (var i=0; i<searchIndexQuery.length; i++) {
 			addQuery(searchStringQuery[i], searchIndexQuery[i], !searchBoolQuery[i]);
+			console.log(searchBoolQuery[i]);
 		}
 		submitQuery(0);
 		searchYearFromSelect.value = searchYearFromString.length-1;
