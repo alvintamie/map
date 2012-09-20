419917008c4f9171c356fb929f58b7b4f6e91cfd
alvi0010 <alvintamie@gmail.com>
2012-09-20 08:58:55 -0700

    Update calculateDelta.js

--- calculateDelta.js
+++ calculateDelta.js
@@ -1,8 +1,8 @@
 function calculateDelta()
 {
 		var curDate = new Date();
-		var x = (mouseX-canvasPosX);
-		var y = (mouseY-canvasPosY);
+		var x = (mouseX-canvas.offsetLeft);
+		var y = (mouseY-canvas.offsetTop);
 		delta = [5*(x - startCoords[0])/(curDate - pastDate), 5*(y - startCoords[1])/(curDate - pastDate)];
 		startCoords = [x,y];
 		pastDate = curDate;
