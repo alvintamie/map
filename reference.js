f4cd020608e1b59fb1f4d42a7fa431279c56b58a
alvi0010 <alvintamie@gmail.com>
2012-09-22 05:02:50 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -9,12 +9,19 @@ var referencePosX;
 var referencePosY;
 var referenceWidth;
 var referenceHeight;
-var overflowX = 1;
 
 var abstractRefHeight = new Array();
 var abstractRefState = new Array();
 var abstractRefMode = new Array();
 var abstractRefTotal = 20;
+var contentReference;
+var showReferenceinMap = 1;
+var showReferenceHref;
+var divCountryDistributionReference;
+var modeCountryDistributionReference = 0;
+var modeCountryTypeReference = 0;
+var hrefCountryTypeReference;
+var hrefCDR;
 
 function initializeReference() {
 	divReference = document.getElementById("windowReference");
@@ -26,52 +33,88 @@ function initializeReference() {
 	//console.log ("before");
 	//setTimeout(insertReference, 3000);
 	//console.log("after");
-	var temp = document.createElement('div');
+	contentReference = document.createElement('div');
 	divReference.appendChild(temp);
-	temp.setAttribute('id', "contentReference");
-	temp.style.position = 'absolute';
-	temp.style.top = topbarHeight-9 + 'px';
-	temp.style.left = 1 + 'px';
-	temp.style.width = referenceWidth-2 + 'px';
-	temp.style.height = referenceHeight-topbarHeight+7 +'px';
-	temp.style['overflow-x'] = 'hidden';
-	temp.style['overflow-y'] = 'auto';
-
-}
-
-function mouseDownReference(e){
-	divReference.style['z-index'] = zIndex;
-	zIndex += 1;
-	if(e.clientY-divReference.offsetTop<topbarHeight) {
-		if (e.clientX-divReference.offsetLeft<=parseInt(divReference.style.width)-minimizePosWidth) {
-			isMouseDownReference=true;
-			lastMouseX = e.clientX;
-			lastMouseY = e.clientY;
+	contentReference.setAttribute('id', "contentReference");
+	contentReference.style.position = 'absolute';
+	contentReference.style.top = topbarHeight-9 + 'px';
+	contentReference.style.left = 1 + 'px';
+	contentReference.style.width = referenceWidth-2 + 'px';
+	contentReference.style.height = referenceHeight-topbarHeight+7 +'px';
+	contentReference.style['overflow-x'] = 'hidden';
+	contentReference.style['overflow-y'] = 'auto';
+	
+	showReferenceHref = document.createElement('a');
+	showReferenceHref.href = "#";
+	showReferenceHref.onclick = function () {
+		if  (showReferenceinMap==0) {
+			showReferenceinMap = 1;
+			showResult(0, citedbyObject);
+			showReferenceHref.textContent = "Hide documents in map";
 		}
-		else if (e.clientX-divReference.offsetLeft>parseInt(divReference.style.width)-minimizePosWidth) {
-			referenceIncrement *= -1;
-			changeViewReference();
+		else {
+			showReferenceinMap = 0;
+			clearCanvasObject();
+			showReferenceHref.textContent = "Show documents in map";
 		}
 	}
+	showReferenceHref.textContent = "Hide documents in map";
+	
+	divCountryDistributionReference = document.createElement('div');
+	divCountryDistributionReference.style.background = 'yellow';
+	divCountryDistributionReference.style.position = 'absolute';
+	divCountryDistributionReference.style.width = '200px';
+	divCountryDistributionReference.style.height = '300px';
+	divCountryDistributionReference.style.top = citedByPosY + 'px';
+	divCountryDistributionReference.style.left = citedByPosX-parseInt(divCountryDistributionReference.style.width) + 'px';
+	divCountryDistributionReference.style['z-index'] = 0;
+	divCountryDistributionReference.style.overflow = 'hidden';
+	divCountryDistributionReference.style.display = 'none';
+	document.body.appendChild(divCountryDistributionReference);
+	
+	hrefCDR = document.createElement('a');
+	hrefCDR.textContent = "View country distribution";
+	hrefCDR.href = "#";
+	hrefCDR.onclick = function () {
+		showReferenceCountryDistribution();
+		if (modeCountryDistributionReference==0)
+			hrefCDR.textContent = "View country distribution";
+		else hrefCDR.textContent = "Hide country distribution";
+	};
 }
 
-function updateReference () {
-	if (readyRef==1) {
-		for (var i=0; i<numberRef; i++) {
+function updateReference (rObject, rMode) {
+	removecontentReferenceChild();
+	if (rObject.length>0) {
+		contentReference.appendChild(showReferenceHref);
+		contentReference.appendChild(document.createElement('br'));
+		if (rMode==1) {
+			var temp = document.createElement('a');
+			temp.href = "#";
+			temp.onclick = function () {updateReference(referenceObject, 0);};
+			temp.textContent = "Show all result";
+			contentReference.appendChild(temp);
+			contentReference.appendChild(document.createElement('br'));
+		}
+
+		contentReference.appendChild(hrefCDR);
+		contentReference.appendChild(document.createElement('br'));
+
+		for (var i=0; i<rObject.length; i++) {
 			var temp = document.createElement('div');
 			document.getElementById("contentReference").appendChild(temp);
 			temp.setAttribute('id', "Reference" + i);
 			temp.style.position = 'relative';
 			temp.style.left = 3 + 'px';
+			insertReference(rObject, i);
 		}
 	}
-	else if (readyRef==2) {
-		document.getElementById("contentReference").innerHTML = "No information about the reference is available.";
-		updateRelevantDocument();
+	else {
+		document.getElementById("contentReference").innerHTML = "There is no data for reference.";
 	}
 }
 
-function insertReference(i) {
+function insertReference(rObject, i) {
 	var temp = document.createElement("IMG");
 	temp.setAttribute('id', "Reference" + i + "_image");
 	temp.src = imgExpand.src;
@@ -79,23 +122,66 @@ function insertReference(i) {
 	temp.onclick = function () {showAbstractRef(i);};
 	document.getElementById("Reference"+i).appendChild(temp);
 	temp = document.createElement("a");
-	temp.href = "javascript:window.open('" + referenceObject[i].url + "')";
-	temp.textContent = referenceObject[i].title;
-	//temp.setAttribute('onclick', 'window.open(temp.href)');
+	temp.onclick = function () {
+		if (abstractRefMode[i]==0) {
+			showAbstractRef(i);
+			highlight(rObject[i]);
+		}
+	};
+	temp.href = "#";
+	temp.textContent = i + " " + rObject[i].title;
+	temp.onclick = function () {
+		if (abstractRefMode[i]==0) {
+			showAbstractRef(i);
+			highlight(rObject[i]);
+		}
+	};
 	document.getElementById("Reference"+i).appendChild(temp);
 	temp = document.createElement('div');
 	document.getElementById("Reference"+i).appendChild(temp);
-	temp.innerHTML = referenceObject[i].Abstract;
 	temp.setAttribute('id', "Reference" + i + "_abstract");
 	temp.style.position = 'relative';
 	temp.style.left = 18 + 'px';
 	temp.style.width = referenceWidth - 45 + 'px';
+	if (rObject[i].url) {
+		var temp2 = document.createElement('a');
+		temp2.textContent = "Show in Scopus";
+		temp2.href = "javascript:window.open('" + rObject[i].url + "')";
+		temp.appendChild(temp2);
+		temp.appendChild(document.createElement('br'));
+	}
+	if (rObject[i].Abstract)
+		temp.appendChild(document.createTextNode(rObject[i].Abstract));
+	else temp.appendChild(document.createTextNode("Abstract not available"));
 	temp.style.overflow = 'hidden';
 	abstractRefHeight[i] = temp.clientHeight;
 	temp.style.height = 0 + 'px';
 	//temp.style.display = 'none';
 	abstractRefState[i] = 0;
 	abstractRefMode[i] = 0;
+}
+
+function showReferenceCountryDistribution() {
+	if (modeCountryDistributionReference==0) {
+		modeCountryDistributionReference = 1;
+		divCountryDistributionReference.style.display = 'block';
+	}
+	else {
+		modeCountryDistributionReference = 0;
+		divCountryDistributionReference.style.display = 'none';
+	}
+}
+
+function showOverallCountryReference(crObject) {
+	while (divCountryDistributionReference.firstChild) {
+		divCountryDistributionReference.removeChild(divCountryDistributionReference.firstChild);
+	}
+	divCountryDistributionReference.appendChild(hrefCountryTypeReference);
+	divCountryDistributionReference.appendChild(document.createElement('br'));
+	for (var i=0; i<crObject.length; i++) {
+		divCountryDistributionReference.appendChild(document.createTextNode(crObject[i].name + " : " + crObject[i].hitCount));
+		divCountryDistributionReference.appendChild(document.createElement('br'));
+	}
 }
 
 function showAbstractRef(i) {
@@ -133,8 +219,28 @@ function contractAbstractRef(i) {
 	//else if (abstractRefMode[i]==0)
 		//document.getElementById("Reference" + i + "_abstract").style.display = 'none';
 }
+
+function mouseDownReference(e){
+	divReference.style['z-index'] = zIndex;
+	divCountryDistributionReference.style['z-index'] = zIndex;
+	zIndex += 1;
+	if(e.clientY-divReference.offsetTop<topbarHeight) {
+		if (e.clientX-divReference.offsetLeft<=parseInt(divReference.style.width)-minimizePosWidth) {
+			isMouseDownReference=true;
+			lastMouseX = e.clientX;
+			lastMouseY = e.clientY;
+		}
+		else if (e.clientX-divReference.offsetLeft>parseInt(divReference.style.width)-minimizePosWidth) {
+			referenceIncrement *= -1;
+			changeViewReference();
+		}
+	}
+}
 	
 function changeViewReference() {
+	modeCountryDistributionReference = 0;
+	divCountryDistributionReference.style.display = 'none';
+	hrefCDR.textContent = "View country distribution";
 	if (referenceStatus <= 0 && referenceIncrement==-1) {
 		referenceStatus = 1;
 	}
@@ -181,6 +287,9 @@ function referenceDisplacement(e){
 			divReference.style.top="0px";
 			referencePosY = 0;
 		}
+		divCountryDistributionReference.style.top = referencePosY + "px";
+		divCountryDistributionReference.style.left = referencePosX-parseInt(divCountryDistributionReference.style.width) + 'px';
+		
 		/*
 		if(divReference.offsetLeft<0){
 			divReference.style.left="0px";
