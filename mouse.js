ecc76e9965204c88eec2406e0d751881a9567d11
alvi0010 <alvintamie@gmail.com>
2012-09-23 01:30:04 +0800

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -61,20 +61,3 @@ function scroll(e){
 			}
 	}
 
-canvas.addEventListener('dblclick', function(){ 
-
-  // Some dazzling stuff happens be here
-  	if(_readyScroll==1 && readyScroll==1 && mouseInCanvas){
-			multiplier=1;
-			timeBefore = 0;
-			if(zoom<2){
-				readyScroll=-1;
-				triangleSize=60;
-			}
-			mouseX=canvas.offsetLeft+canvas.width/2;
-			mouseY=canvas.offsetTop+canvas.height/2;
-			deltaMouseX = mouseX - canvas.offsetLeft;
-			deltaMouseY = mouseY - canvas.offsetTop;
-			}
-
-});
