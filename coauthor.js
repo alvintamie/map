a215d0e9ff96a1a9fe3dfcfa622a09fa9dca2beb
alvi0010 <alvintamie@gmail.com>
2012-09-23 04:48:39 +0800

    Update coauthor.js

--- coauthor.js
+++ coauthor.js
@@ -218,7 +218,7 @@ function showOverallCountryCoAuthor(caObject) {
 		divCountryDistributionCoAuthor.appendChild(document.createTextNode("	"));
 		var temp = document.createElement('a');
 		//temp.href = "#";
-		temp.href = "javascript:getCoauthorsFilter('"+caObject[i].name+"');";
+		temp.href = "javascript:getCoauthorsFilter(new Array('"+caObject[i].name+")');";
 		temp.onclick = function () {getCoauthorsFilter(tempString)};
 		temp.textContent = "focus to this country";
 		divCountryDistributionCoAuthor.appendChild(temp);
