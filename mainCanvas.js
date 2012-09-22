27b26fc4c6c9918defa9632a8d451f5da66996ac
alvi0010 <alvintamie@gmail.com>
2012-09-23 04:49:19 +0800

    Update mainCanvas.js

--- mainCanvas.js
+++ mainCanvas.js
@@ -14,7 +14,7 @@ function initializeMainCanvas(){
 		showInfoObj.style["border-radius"] = "10px";
 		showInfoObj.style["-moz-border-radius"] = "10px";
 		showInfoObj.style["-webkit-border-radius"] = "10px";
-		showInfoObj.style.margin = 5 auto;
+		showInfoObj.style.margin = "5 auto";
 		
 		
 		canvas = document.getElementById('canvas');
