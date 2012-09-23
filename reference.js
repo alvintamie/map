dc0f9470a207b24b40d715f90f4352048ea4d7e1
alvi0010 <alvintamie@gmail.com>
2012-09-24 05:14:45 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -222,7 +222,7 @@ function showOverallCountryReference(crObject) {
 	//divCountryDistributionReference.appendChild(document.createElement('br'));
 	for (var i=0; i<crObject.length; i++) {
 		var temp = document.createElement('a');
-		temp.href = "javascript:focusToCountryReference('"+crObject[i].name+"');";
+		temp.href = "javascript:focusToCountryReference('"+crObject[i].name+"')";
 		temp.textContent = crObject[i].name;
 		temp.style.textDecoration = 'none';
 		divCountryDistributionReference.appendChild(temp);
