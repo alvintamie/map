e4505988be599777e1b4d586713c6f17cc3791cf
alvi0010 <alvintamie@gmail.com>
2012-09-24 03:20:22 +0800

    Update initialize.js

--- initialize.js
+++ initialize.js
@@ -60,7 +60,7 @@ var obj_dis_x;
 	imgObject[8].src = 'https://raw.github.com/alvi0010/map/master/image/tabButton_search1.png';
 	imgObject[9].src = 'https://raw.github.com/alvi0010/map/master/image/tabButton_reference1.png';
 	imgObject[10].src = 'https://raw.github.com/alvi0010/map/master/image/tabButton_citedBy1.png';
-	imgObject[11].src = 'https://raw.github.com/alvi0010/map/master/image/tabButton_relevantDocument1;
+	imgObject[11].src = 'https://raw.github.com/alvi0010/map/master/image/tabButton_relevantDocument1';
 	imgObject[12].src = 'https://raw.github.com/alvi0010/map/master/image/tabButton_coAuthor1.png';
 	imgObject[13].src = 'https://raw.github.com/alvi0010/map/master/image/tabButton_search1.png';
 	//FOR LOGO:
