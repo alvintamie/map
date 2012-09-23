72f6d88cd1f82a26ee58d84b84ef7f04faad8ca1
alvi0010 <alvintamie@gmail.com>
2012-09-23 10:21:09 +0800

    Update mainCanvas.js

--- mainCanvas.js
+++ mainCanvas.js
@@ -53,9 +53,9 @@ function initializeMainCanvas(){
 		canvas.addEventListener('dblclick', function(){ 
 
   // Some dazzling stuff happens be here
-  			showResult(0,countryRelevantDocument);
-  			console.log("db click");
-  			console.log(countryRelevantDocument);
+  	//		showResult(0,countryRelevantDocument);
+  	//		console.log("db click");
+  	//		console.log(countryRelevantDocument);
   			if(_readyScroll==1 && readyScroll==1 && mouseInCanvas){
 			multiplier=1;
 			timeBefore = 0;
