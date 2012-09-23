d2a52be11da1282cab47d17cb745583c9be5815c
alvi0010 <alvintamie@gmail.com>
2012-09-23 15:11:26 +0800

    Update search.js

--- search.js
+++ search.js
@@ -264,7 +264,7 @@ function updatecontentSearchQuery() {
 		searchField.appendChild(document.createElement('br'));
 		
 		searchField.appendChild(document.createTextNode("Sort the document by "));
-		searchField.appendChild(searchSortBy);
+		searchField.appendChild(SearchSortBySelect);
 		searchField.appendChild(document.createElement('br'));
 		
 		var searchSubmitButton = document.createElement('button');
