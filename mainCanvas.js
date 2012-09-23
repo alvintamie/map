151706db6a92f8183764b147a83a631add1c5b68
alvi0010 <alvintamie@gmail.com>
2012-09-23 18:13:04 +0800

    Update mainCanvas.js

--- mainCanvas.js
+++ mainCanvas.js
@@ -103,7 +103,7 @@ function renderCanvas(){
 			ready_x_n=ready_x-img[zoom].width;
 			ctx.drawImage(img[zoom],ready_x_n,imageCoords[1]); }
 			//displayCountry();
-			if (highlightObj.status == 1) renderHighlight();
+			if (highlightObjStatus == 1) renderHighlight();
 		//	setDivPosition(mainArticleDiv, canvas.offsetLeft + canvas.width + 20, canvas.offsetTop + 20);
 	}
 }
