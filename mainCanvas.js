851f853f21073dc2ee2e1828ad9f370a8553ba09
alvi0010 <alvintamie@gmail.com>
2012-09-23 20:02:57 +0800

    Update mainCanvas.js

--- mainCanvas.js
+++ mainCanvas.js
@@ -23,7 +23,7 @@ function initializeMainCanvas(){
 		mainArticleDiv.style.position = "absolute";
 		mainArticleDiv.style.paddingLeft = "5px";
 		mainArticleDiv.style.width = "500px";
-		mainArticleDiv.innerHTML = "test";
+	//	mainArticleDiv.innerHTML = "test";
 	//	setDivInnerHTML(mainArticleDiv, authorObject);
 		document.body.appendChild(mainArticleDiv);
 		
